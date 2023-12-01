e4d44e24ef2a5d25d36a3fe282deec934351ecdf
Elijah Cravens <elijahcravens46@gmail.com>
2023-11-30 20:33:00 -0700

    Setup initial game structure
    - Implemented Player object factory
    - Created Gameboard object with array representation
    - Developed Game Controller for managing game flow
    - Established basic console game functionality
    - Added example usage for player turns and game state

--- script.js
+++ script.js
@@ -1,1 +1,83 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.container')
+
+// Player Object Factory
+const Player = (name, token) => {
+	return { name, token }
+}
+
+// Gameboard Object
+const Gameboard = (() => {
+	// Set initial state of the board
+	const board = Array(9).fill('')
+
+	const getBoard = () => board
+
+	const makeMove = (index, token) => {
+		if (board[index] === '') {
+			board[index] = token
+			return true // successful move
+		}
+
+		return false // invalid move
+	}
+
+	// Reset the board
+	const resetBoard = () => {
+		for (let i = 0; i < board.length; i++) {
+			board[i] = ''
+		}
+	}
+
+	return { getBoard, makeMove, resetBoard }
+})()
+
+// Game controller object
+const GameController = (() => {
+	let currentPlayer
+	let winner
+
+	const startGame = (player1, player2) => {
+		currentPlayer = player1
+		winner = null
+	}
+
+	const switchPlayer = () => {
+		currentPlayer = currentPlayer === player1 ? player2 : player1
+	}
+
+	const checkForWinner = () => {
+		// TODO: add logic to check for winner
+	}
+
+	const playTurn = index => {
+		if (!Gameboard.makeMove(index, currentPlayer.token)) return false
+
+		if (checkForWinner()) {
+			winner = currentPlayer
+		} else {
+			switchPlayer()
+		}
+
+		return true
+	}
+
+	const getWinner = () => winner
+
+	return { startGame, playTurn, getWinner }
+})()
+
+// Example usage
+const player1 = Player('John', 'X')
+const player2 = Player('Steve', 'O')
+
+GameController.startGame(player1, player2)
+
+// Example turns
+GameController.playTurn(0)
+GameController.playTurn(4)
+GameController.playTurn(1)
+
+console.log(Gameboard.getBoard())
+console.log(GameController.getWinner())
+console.log(`${player1.name} or ${player2.name} could be the winner`)
+console.log({player1, player2})
